3b3b645951709d6a7343b561345725190b3e1df6
Milan Mahat <milan.mahat.739@gmail.com>
2024-07-16 12:33:27 +0545

    Updated dynamodb code

--- Lambda/code/lambda_function.py
+++ Lambda/code/lambda_function.py
@@ -54,24 +54,36 @@ def lambda_handler(event, context):
     print(event)
     dynamodb = boto3.resource('dynamodb')
 
+    result = "Invalid service_type, table_name, or record."
+    
     try:
+        print("Trying to load from query params")
         query_params = event.get('queryStringParameters', {})
         service_type = query_params.get('service_type')
         table_name = query_params.get('table_name')
         record = json.loads(query_params.get('record', '{}'))
-
-        if service_type == 'create_table' and table_name:
-            result = create_table(dynamodb, table_name)
-        elif service_type == 'insert_record' and table_name and record:
-            result = insert_record(dynamodb, table_name, record)
-        elif service_type == 'get_records' and table_name:
-            result = get_records(dynamodb, table_name)
-        else:
-            result = "Invalid service_type, table_name, or record."
-
+        
     except Exception as e:
-        print(str(e))
-        result = "Invalid service_type, table_name, or record."
+        print(f"Failed to load from query params: {str(e)}")
+        service_type = None
+        table_name = None
+        record = None
+
+    if not service_type:
+        try:
+            print("Trying to load from event")
+            service_type = event.get('service_type')
+            table_name = event.get('table_name')
+            record = json.loads(event.get('record', '{}'))
+        except Exception as e:
+            print(f"Failed to load from event: {str(e)}")
+    
+    if service_type == 'create_table' and table_name:
+        result = create_table(dynamodb, table_name)
+    elif service_type == 'insert_record' and table_name and record:
+        result = insert_record(dynamodb, table_name, record)
+    elif service_type == 'get_records' and table_name:
+        result = get_records(dynamodb, table_name)
 
     return {
         'statusCode': 200,
